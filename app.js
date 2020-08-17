fccfbbad6a87ed1890dbf928e356618d1305e36d
nirav6 <nirav@alfangroup.com>
2020-08-16 23:24:58 -0400

    add front-end

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const feathers = require('@feathersjs/feathers');
-const app = feathers();
+const express = require('@feathersjs/express');
+const socketio = require('@feathersjs/socketio');
+
+const app = express(feathers());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+
+app.configure(express.rest());
+app.configure(socketio);
 
 class MessageService {
   constructor() {
@@ -15,14 +25,22 @@ class MessageService {
       id: this.messages.length,
       text: data.text,
     };
-
     this.messages.push(message);
     return message;
   }
 }
 
-app.use('messages', new MessageService());
+app.use('/messages', new MessageService());
+app.use(express.errorHandler());
 
-app.service('messages').on('created', (message) => {
-  console.log('new message has been created!');
+app.on('connection', (connection) => {
+  app.channel('everybody').join(connection);
+});
+
+app.listen(3000).on('listening', () => {
+  console.log('feathers server listening on local host port 3000');
+});
+
+app.service('messages').create({
+  text: 'Hello new message here!',
 });
